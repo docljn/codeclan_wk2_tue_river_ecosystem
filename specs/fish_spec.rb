f7ddc2970f5735411bca649e5d7c1efada3a3f75
DocLJN <ljnoble@gmail.com>
2017-11-28 20:14:15 +0000

    Test fish has name.

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -6,10 +6,16 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative("../fish")
 
 class TestFish < MiniTest::Test
-
-
 # TEST FUNCTION NAMING
 # must start with "test_"
+  def setup
+    @carp = Fish.new("carp")
+  end
+
+
+  def test_fish_has_name
+    assert_equal("carp", @carp.name)
+  end
 
 
 end
